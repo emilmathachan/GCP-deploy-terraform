89bc08161217e38e26e6675a6f5c53a8bfd732df
emilmathachan <emilmathachan18@gmail.com>
2025-09-02 16:48:28 +0530

    fix

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "google" {
   project     = "proud-climber-469712-j0"
   region      = "us-central1"
   credentials = ".github/workflows/terraform.yml"
-
 }
 resource "google_compute_instance" "default" {
   name         = "my-vm"

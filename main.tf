63704db788285a4e522ded94ad8c06f56493ac53
emilmathachan <emilmathachan18@gmail.com>
2025-09-02 17:47:05 +0530

    fix

--- main.tf
+++ main.tf
@@ -3,3 +3,19 @@ provider "google" {
   region      = "us-central1"
   credentials = ".github/workflows/terraform.yml"
 }
+resource "google_compute_instance" "default" {
+  name         = "my-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-latest"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}

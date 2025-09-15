f2bbaf448d1eeec263de893f940bd89d28ab902d
emilmathachan <emilmathachan18@gmail.com>
2025-09-15 12:31:26 +0530

    fix

--- main.tf
+++ main.tf
@@ -3,9 +3,3 @@ provider "google" {
   region      = "us-central1"
   credentials = ".github/workflows/terraform.yml"
 }
-resource "google_storage_bucket" "static" {
-  name          = "test_bucket"
-  location      = "US"
-  storage_class = "STANDARD"
-  uniform_bucket_level_access = true
-}
